e6222d0b916b5410102fbb230bdc720d9cab439f
padrisimo <adrianfernandezchamizo@gmail.com>
2019-10-30 04:14:06 +0100

    getTopAppsByHost method basic algorithm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Kids from "./Kids.js";
+const data = require("./moks/hostAppData.json");
 
 const App = children =>
   `<div>
@@ -13,6 +14,16 @@ App.data = {
   title: "Vanilla Apdex",
   subTitle: "an delightful thing to look at"
 };
+
+const getTopAppsByHost = function(data) {
+  return data
+    .sort(function(a, b) {
+      return a.apdex - b.apdex;
+    })
+    .slice(-25)
+    .reverse();
+};
+console.log(getTopAppsByHost(data));
 
 const root = document.querySelector("#root");
 root.innerHTML = App(Kids);
