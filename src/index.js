7dfd303f5d17202d91afc98b2f38c5c8f3d3f47b
padrisimo <adrianfernandezchamizo@gmail.com>
2019-10-30 04:58:49 +0100

    getHostList

--- src/index.js
+++ src/index.js
@@ -15,15 +15,28 @@ App.data = {
   subTitle: "an delightful thing to look at"
 };
 
-const getTopAppsByHost = function(data) {
+const getHostList = data => {
+  const rowList = [];
+  data.map(item => rowList.push(...item.host));
+  return [...new Set(rowList)];
+};
+
+const getTopAppsByHost = function(data, host) {
   return data
+    .filter(item => item.host.includes(host))
     .sort(function(a, b) {
       return a.apdex - b.apdex;
     })
     .slice(-25)
     .reverse();
 };
-console.log(getTopAppsByHost(data));
+
+console.log(
+  "getTopAppsByHost",
+  getTopAppsByHost(data, "7e6272f7-098e.dakota.biz")
+);
+
+console.log("getHostList", getHostList(data));
 
 const root = document.querySelector("#root");
 root.innerHTML = App(Kids);
